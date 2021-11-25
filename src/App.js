211ca310ec9e0912d2e9f01a67f3de678b0b115a
dabliu682 <dabliu.682@gmail.com>
2021-11-24 22:02:40 -0500

    Final capitulo introduccion y basicos de react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import React from "react";
+import React, {Fragment, useState } from "react";
+import Header from "./componentes/Header";
+import Footer from "./componentes/Footer";
+import Producto from "./componentes/productos";
+import Carrito from "./componentes/Carrito";
 
 function App() {
+
+  const [productos, guardarProductos] = useState(
+    [
+      {id:1, nombre: 'Camisa React.js', precio: 50},
+      {id:2, nombre: 'Camisa Vue.js', precio: 40},
+      {id:3, nombre: 'Camisa Node.js', precio:30},
+      {id:4, nombre: 'Camisa Angular.js', precio: 20},
+      {id:5, nombre: 'Camisa Symfony', precio: 10},
+    ]
+  )
+
+  const [carrito, agregarProducto] = useState([])
+
+  
+  // obtener año actual
+  let anio = new Date().getFullYear();
+
   return (
-    <div className="App">
-      <h1>Hola mundo</h1>
-    </div>
+    <Fragment>
+      < Header 
+        titulo="Tienda virtual"
+      />
+
+      <h1>Lista de productos</h1>
+      {productos.map(producto =>(
+        <Producto 
+          key={producto.id}
+          producto={producto}
+          productos={productos}
+          carrito={carrito}
+          agregarProducto={agregarProducto}
+        />
+      ))}
+
+      < Carrito
+        carrito={carrito}
+        agregarProducto={agregarProducto}
+       />
+      
+      < Footer 
+        anio = {anio}
+      />
+    </Fragment>
   );
 }
 
---
+++ src/componentes/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = ({anio}) =>( 
+    <footer>
+        <p>Todos los derechos reservados &copy; {anio} </p>
+    </footer>
+);
+ 
+export default Footer;
